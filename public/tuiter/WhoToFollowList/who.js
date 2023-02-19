5cadea2fe6d9b1b22b6a72e1c59961ea3815744e
LokLambert18 <you@example.com>
2023-02-19 11:31:11 -0500

    A5 Bootstrap Spring 2023

---
+++ public/tuiter/WhoToFollowList/who.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        userName: 'Java',
+        handle: '@Java',
+        avatar: '../assets/images/java-logo.png',
+        nameIcon: '<span class="fa-stack fa-1x wd-stack-correction wd-verified-override"> <i class="fas fa-circle fa-stack-1x"></i> <i class="fas fa-check fa-2xs fa-stack-1x fa-inverse color-black"></i> </span>',
+    },
+    {
+        userName: 'Relativity Space',
+        handle: '@relativityspace',
+        avatar: '../assets/images/relativity-logo.png',
+        nameIcon: '<span class="fa-stack fa-1x wd-stack-correction wd-verified-override"> <i class="fas fa-circle fa-stack-1x"></i> <i class="fas fa-check fa-2xs fa-stack-1x fa-inverse color-black"></i> </span>',
+    },
+    {
+        userName: 'Virgin Galactic',
+        handle: '@virgingalactic',
+        avatar: '../assets/images/virgin-logo.png',
+        nameIcon: '<span class="fa-stack fa-1x wd-stack-correction wd-verified-override"> <i class="fas fa-circle fa-stack-1x"></i> <i class="fas fa-check fa-2xs fa-stack-1x fa-inverse color-black"></i> </span>',
+    },
+    {
+        userName: 'NASA',
+        handle: '@NASA',
+        avatar: '../assets/images/NASA-logo.png',
+        nameIcon: '<span class="fa-stack fa-1x wd-stack-correction wd-verified-override"> <i class="fas fa-circle fa-stack-1x"></i> <i class="fas fa-check fa-2xs fa-stack-1x fa-inverse color-black"></i> </span>',
+    },
+    {
+        userName: 'Tesla',
+        handle: '@Tesla',
+        avatar: '../assets/images/Tesla_t.png',
+        nameIcon: '<span class="fa-stack fa-1x wd-stack-correction wd-verified-override"> <i class="fas fa-circle fa-stack-1x"></i> <i class="fas fa-check fa-2xs fa-stack-1x fa-inverse color-black"></i> </span>',
+    },
+]
